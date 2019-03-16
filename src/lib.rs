6494aa8812aa5019626a492a38d1a6f678f54e1f
daniel <djawna@gmail.com>
2019-03-16 10:46:09 +0100

    change the functions to methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,17 @@ pub mod core {
     /// this is the principal rendering interface
     pub trait IRenderer{
 
-        /// loads texture into the graphicssystem, and returns id of it
-        fn create_texture(filename: &str) -> Result<u32,String>;
 
-        fn paint_rect2d(x: f32,y: f32,width: f32, height: f32,red: u8, green: u8, blue: u8, alpha: u8)-> Option<String>;
+
+        /// loads texture into the graphicssystem, and returns id of it
+        fn create_texture(&self, filename: &str) -> Result<u32,String>;
+
+        fn paint_rect2d(&self,x: f32,y: f32,width: f32, height: f32,red: u8, green: u8, blue: u8, alpha: u8)-> Option<String>;
 
 
         // displays a texture at the target_x and target_y coordinates
-        fn draw_texture2d(  texture_id   : u32, 
+        fn draw_texture2d(  &self,
+                            texture_id   : u32, 
                             source_x     : u32,
                             source_y     : u32, 
                             source_width : u32, 
@@ -19,6 +22,6 @@ pub mod core {
                             target_width : f32, 
                             target_height: f32) -> Option<String>;
 
-        fn clear(red: u8, green: u8, blue: u8)-> Option<String>; 
+        fn clear(&self,red: u8, green: u8, blue: u8)-> Option<String>; 
     } 
 }
